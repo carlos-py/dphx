0a6f78fda62c834b82bf74bce3503edfc898166f
MOZGIII <mike-n@narod.ru>
2016-03-26 00:05:01 +0300

    Fixed bug: tags are space-separated

--- config.go
+++ config.go
@@ -3,5 +3,5 @@ package dphx
 // AppConfig loads and saves config.
 type AppConfig struct {
 	SSH       SSHConfig
-	LocalAddr string `envconfig:"SOCKS_ADDR",default:"127.0.0.1:1080"`
+	LocalAddr string `envconfig:"SOCKS_ADDR" default:"127.0.0.1:1080"`
 }
